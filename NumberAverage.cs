b8f92779fe65ca2d7a91ac20fd1ef52c6d9e9e62
AthiraMohan14 <73377097+AthiraMohan14@users.noreply.github.com>
2020-11-03 11:25:23 +0530

    ss
    ss

---
+++ NumberAverage.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Linq;
+
+namespace NumberAverage
+{
+	public class NumberAverage
+	{
+		public static void Main(string[] args)
+		{
+			try
+			{
+				Console.WriteLine("Enter the number of values: ");
+				// get size of array
+				int length = System.Convert.ToInt32(Console.ReadLine());
+				// declare array for the given size
+				int[] numbers = new int[length];
+				
+				//get the values of the array from the user
+				Console.WriteLine("Enter the values:");
+				string[] numberString = Console.ReadLine().Split(' ');
+				for(int i=0; i<length; i++){
+					numbers[i] = System.Convert.ToInt32(numberString[i]);
+				}
+				
+				//call FindAverage() method to calculate average and receive string response
+				string result = FindAverage(numbers);
+				//print the result
+				Console.WriteLine();
+				Console.WriteLine(result);
+			}
+			catch (Exception err)
+			{
+				Console.WriteLine(err.Message);
+			}
+		}
+
+		//write here logic to calculate the average an array
+		public static String FindAverage(int[] numbers)
+		{
+			string result = "";
+			//Check whether array is empty
+			if(numbers.Length == 0){
+				result = "Array is Empty";
+			}
+			//Check whether all numbers are positive
+			else if(numbers.Any(x => x < 0)){
+				result = "Negative values in array";
+			}
+			//Calculate average
+			else{
+				int total = 0;
+				foreach(int number in numbers){
+					total += number;
+				}
+				result = "The Average is: " + (total/(numbers.Length));
+			}
+			return result;
+		}
+	}
+}
